6c3eeab2a824c773c7a7632ab6cc1fce6b0873b0
Remy <Remy@REMYI>
2016-12-15 13:14:06 -0800

    Admin viewmodel

---
+++ SeaToSkyCabinsApplication/SeaToSkyCabinsApplication/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeaToSkyCabinsApplication.Models
+{
+    public class Admin
+    {
+        public int UserRoleID { get; set; }
+        public int AdminID { get; set; }
+        public DateTime HireDate { get; set; }
+        public int OfficePhone { get; set; }
+    }
+}

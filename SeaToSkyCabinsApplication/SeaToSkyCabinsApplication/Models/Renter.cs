b6da88a698d1ca1328f4d62e460b87ac002d0559
Carlos <carlokoz@gmail.com>
2016-12-15 13:22:52 -0800

    working on renter signup

--- SeaToSkyCabinsApplication/SeaToSkyCabinsApplication/Models/Renter.cs
+++ SeaToSkyCabinsApplication/SeaToSkyCabinsApplication/Models/Renter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -7,15 +9,22 @@ namespace SeaToSkyCabinsApplication.Models
 {
     public class Renter
     {
+        [Required]
         public User User { get; set; }
         public int RoleID { get; set; }
+        [Key]
         public int RenterID { get; set; }
         public int SecondaryPhone { get; set; }
+        [DataType(DataType.ImageUrl)]
         public string ProfilePicture { get; set; }
         public decimal RenterRating { get; set; }
+        [Required]
         public string PhotoIDNumber { get; set; }
+        [Required]
         public int ChargeCardNumber { get; set; }
+        [Required]
         public int ChargeCardExpiration { get;set; }
+        [Required]
         public int ChargeCardCode { get; set; }
 
     }

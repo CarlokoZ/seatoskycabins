1a178e3c4b6477aa78be9d26c1f1d4ead0a5dee0
Remy <Remy@REMYI>
2016-12-15 13:26:52 -0800

    Owner viewmodel

---
+++ SeaToSkyCabinsApplication/SeaToSkyCabinsApplication/Models/Ownermodel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SeaToSkyCabinsApplication.Models
+{
+    public class Ownermodel
+    {
+        public int UserRoleID { get; set; }
+        public int OwnerID { get; set; }
+        public int SIN { get; set; }
+        public int DirectDepositRouting { get; set; }
+        public int DirectDepositAccount { get; set; }
+    }
+}
